cbe86d491143f22d3fd1a34de9960aa400e41ffb
Cherry <masahiro970513@gmail.com>
2019-05-29 14:20:32 +0900

    346

--- app/src/main/java/com/example/myscheduler/MainActivity.kt
+++ app/src/main/java/com/example/myscheduler/MainActivity.kt
@@ -5,21 +5,30 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import io.realm.Realm
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var realm: Realm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        realm = Realm.getDefaultInstance()
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
