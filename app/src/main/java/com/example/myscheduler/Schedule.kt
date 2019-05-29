c88eb8068d90c90aa743900f3c8aceb2f2bd15a1
Cherry <masahiro970513@gmail.com>
2019-05-29 14:14:41 +0900

    344

--- app/src/main/java/com/example/myscheduler/Schedule.kt
+++ app/src/main/java/com/example/myscheduler/Schedule.kt
@@ -1,4 +1,13 @@
 package com.example.myscheduler
 
-class Schedule {
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import java.util.*
+
+open class Schedule : RealmObject() {
+    @PrimaryKey
+    var id: Long = 0
+    var date: Date = Date()
+    var title: String = ""
+    var detail: String = ""
 }
